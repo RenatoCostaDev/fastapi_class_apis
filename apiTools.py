8ff1762b9f24dd3637041f5e94975870db77fefc
Renato <renato_da_costa2000@yahoo.com.br>
2023-09-30 14:49:33 -0300

    feat:api 07

--- apiTools.py
+++ apiTools.py
@@ -1,8 +1,30 @@
-import urllib.request, json
+from books import *
 
-def get_data_url(url):
-    resposta = urllib.request.urlopen(url)
-    dados = resposta.read()
-    json_data = json.loads(dados)
+def update_id(Books: list, book: Book) -> Book:
+    '''
+    Retorna o livro criado com o "id" correto
+    '''
+    book.id = 1 if len(Books) == 0 else Books[-1].id + 1
+    return book
 
-    return json_data
+    # if len(Books) > 0:
+    #     book.id = Books[-1].id + 1
+    # else:
+    #     book.id = 1
+    # return book
+
+def get_book_id(id: int, Books: list):
+    for book in Books:
+        if book.id == id:
+            return book
+        
+def book_to_update(book_request: Book, Books: list):
+    for i in range(len(Books)):
+        if Books[i].id == book_request.id:
+            Books[i] = book_request
+
+def book_to_delete(id: int, Books: list):
+    for i in range(len(Books)):
+        if Books[i - 1].id == id:
+            Books.pop(i)
+            break
--- main.py
+++ main.py
@@ -1,28 +1,34 @@
-from fastapi import FastAPI, Request, Form
-from fastapi.responses import RedirectResponse
-from fastapi.templating import Jinja2Templates
-
+from fastapi import FastAPI
+from data import Books
 from apiTools import *
+from books import *
 
-app = FastAPI()
-templates = Jinja2Templates(directory='templates')
+app = FastAPI(    
+   title='Api Biblioteca',
+   version='0.0.1',
+   description="Api para uma biblioteca,Obs: Pydantic",
+)
 
-dados_cep = {}
+@app.get('/books')
+async def get_books():
+    return Books
 
-@app.get('/cep')
-async def get_address(request: Request):
+@app.get('/books/{id}')
+async def get_book_by_id(id: int):
+    return get_book_id(id, Books)
 
-    return templates.TemplateResponse(
-        'base.html',
-        {'request': request, 'dados_cep': dados_cep}
-    )
+@app.post('/create-book')
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())
+    Books.append(update_id(Books, new_book))
+    return Books
 
-@app.post('/post-cep')
-async def post_cep(request: Request, cep: str = Form(...)):    
-    url = f'https://viacep.com.br/ws/{cep}/json/'
-    dados_cep = get_data_url(url)
+@app.put('/update-book')
+async def update_book(book_request: BookRequest):
+    book_to_update(book_request, Books)
+    return Books
 
-    return templates.TemplateResponse(
-        'base.html',
-        {'request': request, 'dados_cep': dados_cep}
-    )
+@app.delete('/books/{id}')
+async def delete_book(id: int):
+    book_to_delete(id, Books)
+    return Books
