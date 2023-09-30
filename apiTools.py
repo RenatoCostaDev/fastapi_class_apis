6f1c699ff34e0c1be4ece7c00e4e33b0ed28ef2c
Renato <renato_da_costa2000@yahoo.com.br>
2023-09-30 11:10:56 -0300

    feat:api 04

--- apiTools.py
+++ apiTools.py
@@ -1,141 +1,17 @@
-# get todos clientes sem as avaliações (HOF)
-map_client = lambda client: {
-        'id': client['id'],
-        'nome': client['nome'],
-    }
+import urllib.request, json
 
-def get_formatted_data_basic(lista_clientes: list) -> list:
-    '''
-        Recebe: lista de dict de clients
-        Retorna: lista de dict de clients sem  os dados/keys: peso, altura e as avaliações
-    '''
-    data_basic = list(map(map_client, lista_clientes))
-    return data_basic
+def get_data_url(url):
+    resposta = urllib.request.urlopen(url)
+    dados = resposta.read()
+    json_data = json.loads(dados)
+    dados_covid = json_data['data']
 
-# get cliente todas avaliações
-def get_formatted_data(lista_clientes: list, id:int):
-    data_basic = []
-    for client in lista_clientes:
-        if client['id'] == id:
-            data_basic.append({
-                'id': client['id'],
-                'nome': client['nome'],
-                'altura': client['altura'],
-                'peso': client['peso'],
-                'avaliações': client['avaliações']
-            })
-    return data_basic
+    return dados_covid
 
-# get cliente com última avaliação
-def get_client_last_measure(lista_clientes: list, id: int):
-    data_basic = []
-    for client in lista_clientes:
-        if client['id'] == id:
-            avaliacoes = client['avaliações']
-            if avaliacoes:  # Verifica se a lista de avaliações não está vazia
-                data_basic.append({
-                    'nome': client['nome'],
-                    'altura': client['altura'],
-                    'peso': client['peso'],
-                    'avaliações': avaliacoes[-1]
-                })
-    return data_basic
+def organize_case_list(case_list):
+    sorted_case_list = sorted(case_list, key=lambda x: x['suspects'], reverse=True)
+    return sorted_case_list
 
-# post: com primeira avaliação 
-def criar_dict_avaliacao(nome, peso, altura, data):
-    # Cálculo
-    if altura <= 0:
-        raise ValueError("A altura deve ser maior que zero.")
-    imc = peso / (altura ** 2)
-    # Condicionais classificação
-    classificacao = ''
-    if imc < 18.5:
-        classificacao = "MAGREZA"
-    elif imc < 25.0:
-        classificacao = "NORMAL"
-    elif imc < 30.0:
-        classificacao = "SOBREPESO I"
-    elif imc < 40.0:
-        classificacao = "OBESIDADE II"
-    else:
-        classificacao = "OBESIDADE GRAVE III"
-
-    # Retorno do dict criado
-    return {
-        'nome': nome,
-        'altura': altura,
-        'peso': peso,
-        'avaliações': [
-          {
-            "data": data,
-            "imc": round(imc, 2),
-            "classificação": classificacao
-          }
-        ]
-    }
-
-def create_new_client(nome, peso, altura, data, lista_clientes):
-    new_client = criar_dict_avaliacao(nome, peso, altura, data)
-    
-    # 1st Ache o maior número id
-    max_id = max(client['id'] for client in lista_clientes) if lista_clientes else 0
-    
-    # Incrementa ele
-    new_id = max_id + 1
-    
-    # Confere se existe algum igual
-    while any(client['id'] == new_id for client in lista_clientes):
-        new_id += 1
-
-    lista_clientes.append({
-        'id': new_id,
-        'nome': nome,
-        'altura': altura,
-        'peso': peso,
-        'avaliações': new_client['avaliações']
-    })
-    
-# update: mudando peso, coloca nova avaliação no []
-def update_dict_avaliacao(id, peso, data, lista_clientes):
-
-    for client in lista_clientes:
-        if client['id'] == id:
-            # Cálculo
-            if client['altura'] <= 0:
-                raise ValueError("A altura deve ser maior que zero.")
-            imc = peso / (client['altura'] ** 2)
-            # Condicionais classificação
-            classificacao = ''
-            if imc < 18.5:
-                classificacao = "MAGREZA"
-            elif imc < 25.0:
-                classificacao = "NORMAL"
-            elif imc < 30.0:
-                classificacao = "SOBREPESO I"
-            elif imc < 40.0:
-                classificacao = "OBESIDADE II"
-            else:
-                classificacao = "OBESIDADE GRAVE III"
-            
-            client['peso'] = peso
-            
-            client['avaliações'].append({
-                "data": data,
-                "imc": round(imc, 2),
-                "classificação": classificacao
-            })
-
-    return lista_clientes
-
-# Delete client
-def delete_client_by_id(id, lista_clientes):
-    id_delete = None
-
-    for index, client in enumerate(lista_clientes):
-        if client['id'] == id:
-            id_delete = index
-
-    if id_delete is not None:
-        del lista_clientes[id_delete]
-
-    return lista_clientes
+def organize_case_list_reverse(case_list):
+    sorted_case_list = sorted(case_list, key=lambda x: x['suspects'], reverse=False)
+    return sorted_case_list
--- main.py
+++ main.py
@@ -1,71 +1,39 @@
-from fastapi import FastAPI, Depends, HTTPException, status
-
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from apiTools import *
-from data import lista_clientes
 
 app = FastAPI()
+templates = Jinja2Templates(directory='templates')
 
-@app.get('/clientes', status_code=status.HTTP_200_OK)
-async def get():
-    try:
-        return get_formatted_data_basic(lista_clientes)
-    except:
-        raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail='Clientes não encontrados'
+@app.get("/covid", response_class=HTMLResponse)
+def read_root(request: Request): 
+    url = 'https://covid19-brazil-api.vercel.app/api/report/v1'
+    dados_covid = get_data_url(url)
+
+    return templates.TemplateResponse(
+        'covid.html',
+        {"request": request, 'dados_covid': dados_covid }
     )
 
-@app.get('/cliente/{id}', status_code=status.HTTP_200_OK)
-async def get_client_by_id(id: int):
-    # return cursos[id]
-    try:
-        return get_formatted_data(lista_clientes, id)
-    except:
-        raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail='Cliente não encontrado'
+@app.get("/covid/desc", response_class=HTMLResponse)
+def read_desc(request: Request):
+    url = 'https://covid19-brazil-api.vercel.app/api/report/v1'
+    dados_covid = get_data_url(url)
+    organized_desc = organize_case_list(dados_covid)
+
+    return templates.TemplateResponse(
+        'desc.html',
+        {"request": request, 'organized_desc': organized_desc }
     )
 
-@app.get('/cliente-last-measure/{id}', status_code=status.HTTP_200_OK)
-async def get_last_measure(id: int):
-    # return cursos[id]
-    try:
-        return get_client_last_measure(lista_clientes, id)
-    except:
-        raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail='Cliente não encontrado'
-    )
+@app.get("/covid/cresc", response_class=HTMLResponse)
+def read_cresc(request: Request):
+    url = 'https://covid19-brazil-api.vercel.app/api/report/v1'
+    dados_covid = get_data_url(url)
+    organized_cresc = organize_case_list_reverse(dados_covid)
 
-@app.post('/cliente-post', status_code=status.HTTP_201_CREATED)
-async def post_client(nome: str, peso: float, altura: float, data: str):
-      try:
-        create_new_client(nome, peso, altura, data, lista_clientes)
-        return lista_clientes
-      except:
-          raise HTTPException(
-        status_code=status.HTTP_409_CONFLICT,
-        detail='Cliente não criado'
-    )          
-
-@app.put('/cliente-update/{id}', status_code=status.HTTP_202_ACCEPTED)
-async def update_client(id: int, peso: float, data: str):
-    try:
-        return update_dict_avaliacao(id, peso, data, lista_clientes)         
-    except:
-        raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail=f'Cliente com o id:{id} não encontrado'
+    return templates.TemplateResponse(
+        'cresc.html',
+        {"request": request, 'organized_cresc': organized_cresc }
     )
-    
-
-@app.delete('/cliente-delete/{id}', status_code=status.HTTP_200_OK)
-async def delete_client(id: int):
-    try:        
-        return delete_client_by_id(id, lista_clientes)
-    except:
-        raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail=f'Curso não encontrado com o id:{id}'
-    )
-    
