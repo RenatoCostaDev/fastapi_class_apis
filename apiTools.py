6101c9c5cdfd1e81f1a68265327596cff90f118b
Renato <renato_da_costa2000@yahoo.com.br>
2023-09-30 14:37:37 -0300

    feat:api 06-extra

--- apiTools.py
+++ apiTools.py
@@ -1,30 +1,8 @@
-def get_by_id(id, lista_clientes):
-    for cliente in lista_clientes:
-        if cliente['id'] == id:
-            return cliente
-        
-def create_cliente_new(nombre, plato_favorito, lista_clientes: list):
-    new_cliente = {
-        'id': len(lista_clientes) + 1,
-        'nombre': nombre,
-        'plato_favorito': plato_favorito
-    }
-    lista_clientes.append(new_cliente)
-    return lista_clientes
+import urllib.request, json
 
-def update_cliente_by_id(nombre, plato_favorito, lista_clientes, id):
-    for cliente in lista_clientes:
-        if cliente['id'] == id:
-            cliente['nombre']=  nombre
-            cliente['plato_favorito']=  plato_favorito
-    
-    return lista_clientes
+def get_data_url(url):
+    resposta = urllib.request.urlopen(url)
+    dados = resposta.read()
+    json_data = json.loads(dados)
 
-def delete_cliente_by_id(lista_clientes, id):
-    lista_clientes.pop(id - 1)
-    new_id = 0
-    for cliente in lista_clientes:
-        new_id +=1
-        cliente['id'] = new_id
-    
-    return lista_clientes
+    return json_data
--- main.py
+++ main.py
@@ -1,71 +1,28 @@
-from fastapi import FastAPI, HTTPException, status, Path
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
+
 from apiTools import *
-from data import lista_clientes
-app = FastAPI(    
-   title='Api Restaurante',
-   version='0.0.1',
-   description="Api com nomes de clientes e seu prato favorito",
-)
 
-@app.get('/clientes',
-        status_code=status.HTTP_200_OK,
-        description='Retorna todos os clientes ou uma lista vazia.',
-        summary='Retorna todos os clientes'
-        )
-async def get_clientes():
-    try:
-        return lista_clientes
-    except:
-        raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail='Clientes não encontrados'
+app = FastAPI()
+templates = Jinja2Templates(directory='templates')
+
+dados_cep = {}
+
+@app.get('/cep')
+async def get_address(request: Request):
+
+    return templates.TemplateResponse(
+        'base.html',
+        {'request': request, 'dados_cep': dados_cep}
     )
 
-@app.get('/cliente/{id}', status_code=status.HTTP_200_OK)
-async def get_cliente_id(id: int = Path(
-    title='ID do curso',
-    description=f'Deve ser de 1 a {len(lista_clientes)}',
-    gt=1,
-    lt=len(lista_clientes) + 1)
-    ):
-    try:
-        resposta = get_by_id(id, lista_clientes)
-        return resposta
-    except:
-        raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail='Cliente não encontrado'
-    )
+@app.post('/post-cep')
+async def post_cep(request: Request, cep: str = Form(...)):    
+    url = f'https://viacep.com.br/ws/{cep}/json/'
+    dados_cep = get_data_url(url)
 
-@app.post('/cliente-create', status_code=status.HTTP_201_CREATED)
-async def create_cliente(nombre: str, plato_favorito: str):
-    try:
-        resposta = create_cliente_new(nombre, plato_favorito, lista_clientes)
-        return resposta
-    except:
-        raise HTTPException(
-        status_code=status.HTTP_409_CONFLICT,
-        detail='Cliente não criado'
-    )   
-
-@app.put('/cliente-update/{id}', status_code=status.HTTP_202_ACCEPTED)
-async def update_cliente(nombre: str, plato_favorito: str, id: int):
-    try:
-        resposta = update_cliente_by_id(nombre, plato_favorito, lista_clientes, id)
-        return resposta
-    except:
-        raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail=f'Cliente com o id:{id} não encontrado'
-    )
-
-@app.delete('/cliente-delete/{id}', status_code=status.HTTP_200_OK)
-async def delete_cliente(id: int):
-    try:        
-        resposta = delete_cliente_by_id(lista_clientes, id)
-        return resposta
-    except:
-        raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail=f'Cliente não encontrado com o id:{id}'
+    return templates.TemplateResponse(
+        'base.html',
+        {'request': request, 'dados_cep': dados_cep}
     )
